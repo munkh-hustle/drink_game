8b4411bcb4afd77de45a5906e5ba772b024fe478
“Munkh-Erdene” <munkh.hustle@gmail.com>
2025-10-27 11:59:25 +0800

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-import 'data/default_cards.dart';
 import 'data/card_collections.dart';
 
 void main() {
